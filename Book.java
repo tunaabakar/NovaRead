6687529253c44bd5d8529ac3789056b75ce4f006
tunaabakar <ridhohidayat11111@gmail.com>
2025-12-02 10:48:39 +0800

    Menambahkan beberapa node seperti node buku, node untuk doubly linked list, lalu node untuk stack dan queue nya, yang siap buat digunakan ntar

---
+++ Book.java
@@ -0,0 +1,26 @@
+public class Book {
+    int id;
+    String title;
+    String author;
+    String genre;
+    double rating;
+
+    public Book(int id, String title, String author, String genre, double rating) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.rating = rating;
+    }
+}
+/*
+ * ==========================================
+ * CLASS: Book
+ * ==========================================
+ * Fungsi: 
+ * Ini adalah "Blueprint" atau node utama data kita.
+ * Class ini cuma buat nyimpen properti buku (Judul, ID, Rating, dll).
+ * Isinya cuma data mentah.
+ * ==========================================
+ */
+//kalo ada yang merasa masih kurang ato mau di tambahin tambahin aja yaaa :)
---
+++ QueueNode.java
@@ -0,0 +1,19 @@
+public class QueueNode {
+    Book data;
+    QueueNode next;
+
+    public QueueNode(Book data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+/*
+ * ==========================================
+ * CLASS: QueueNode
+ * ==========================================
+ * Fungsi:
+ * Node sederhana khusus untuk fitur Antrian (Queue).
+ * Mirip StackNode, tapi dipisah biar kodenya rapi dan jelas mana node buat antriannya sama mana node buat history-nya.
+ * ==========================================
+ */
