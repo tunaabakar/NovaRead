83e1f6fe201f7bd6a1a0185ad6cf598f1fb0e28d
asrofisemet <asrofisazani@gmail.com>
2025-12-07 22:11:10 +0800

    Update Book.java

--- Book.java
+++ Book.java
@@ -4,23 +4,26 @@ public class Book {
     String author;
     String genre;
     double rating;
+    String tag;
+    String desc;
+    String lastDate;
 
-    public Book(int id, String title, String author, String genre, double rating) {
+    public Book(int id, String title, String author, String genre, double rating, String tag, String desc, String lastDate) {
         this.id = id;
         this.title = title;
         this.author = author;
         this.genre = genre;
         this.rating = rating;
+        this.tag = tag;
+        this.desc = desc;
+        this.lastDate = lastDate;
+    }
+
+    public String toString() {
+        return "[" + id + "] " + title + " - " + author + 
+               " | Genre: " + genre + 
+               " | Rating: " + rating + 
+               " | Tag: " + tag +
+               " | Last Read: " + lastDate;
     }
 }
-/*
- * ==========================================
- * CLASS: Book
- * ==========================================
- * Fungsi: 
- * Ini adalah "Blueprint" atau node utama data kita.
- * Class ini cuma buat nyimpen properti buku (Judul, ID, Rating, dll).
- * Isinya cuma data mentah.
- * ==========================================
- */
-//kalo ada yang merasa masih kurang ato mau di tambahin tambahin aja yaaa :)
